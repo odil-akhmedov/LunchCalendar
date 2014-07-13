797d3a99214b20bc712b5d6115fa7e68b03f585b
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-07-12 22:12:24 -0400

    Seems like Grid view started to work

--- src/com/zos/lunchcalendar/MainActivity.java
+++ src/com/zos/lunchcalendar/MainActivity.java
@@ -3,6 +3,7 @@ package com.zos.lunchcalendar;
 import java.util.ArrayList;
 import android.R.string;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.util.Log;
@@ -11,12 +12,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	final String TAG = "MainActivity";
-	private int viewType; //0 - for list view, 1 for grid view
+	private int viewType = 1; // 0 - for list view, 1 for grid view
 
 	CustomAdapter adapter;
 	ArrayList<String> result = new ArrayList<String>();
@@ -30,9 +32,8 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		
-		switch (viewType){
+
+		switch (viewType) {
 		case 0:
 			setContentView(R.layout.activity_main_list);
 			break;
@@ -40,10 +41,10 @@ public class MainActivity extends Activity {
 			setContentView(R.layout.activity_main_grid);
 			break;
 		}
-		
+
 		startParsing();
 		// adapter = new CalendarAdapters(getApplicationContext());
-		
+
 		if (savedInstanceState != null) {
 			Log.v(TAG, "onRestoreInstanceState called");
 			Log.v(TAG, "Values restored in onRestoreInstanceState = "
@@ -51,21 +52,21 @@ public class MainActivity extends Activity {
 		}
 
 	}
-	
+
 	@Override
 	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onRestoreInstanceState(savedInstanceState);
-		viewType = savedInstanceState.getInt("VIEWTYPE");		
+		viewType = savedInstanceState.getInt("VIEWTYPE");
 	}
-	
+
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		// TODO Auto-generated method stub
 		super.onSaveInstanceState(outState);
 		outState.putInt("VIEWTYPE", viewType);
 	}
-	
+
 	protected void onRestart() {
 		// TODO Auto-generated method stub
 		super.onRestart();
@@ -99,8 +100,8 @@ public class MainActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onConfigurationChanged(newConfig);
 	}
-	
-	//Options menu
+
+	// Options menu
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -108,49 +109,54 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@SuppressLint("NewApi")
 	@Override
 	public boolean onOptionsItemSelected(MenuItem menuItem) {
-		Toast.makeText(this, "Next will be " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "Next will be " + menuItem.getTitle(),
+				Toast.LENGTH_SHORT).show();
 		switch (menuItem.getItemId()) {
 		case R.id.listViewMode: // do something
-			viewType = 0; //List
+			viewType = 0; // List
+			// super.recreate();
 			return true;
 		case R.id.gridViewMode:
-			viewType = 1; //Grid
+			viewType = 1; // Grid
+			// super.recreate();
 			return true;
 		default:
 			return super.onOptionsItemSelected(menuItem);
 		}
 	}
 
-
 	public void open(View view) {
 		obj = new JSONCalendarParser(url1, getApplicationContext());
 		obj.fetchJSON();
 		// Toast.makeText(getApplicationContext(), "msg msg",
 		// Toast.LENGTH_SHORT).show();
-		while (obj.parsingComplete);
+		while (obj.parsingComplete)
+			;
 
 		result = obj.getContentFromJson();
 
 		menuForMonth = obj.getMenuFromJson();
-		switch (viewType){
+		switch (viewType) {
 		case 0:
 			CustomAdapter adapter = new CustomAdapter(this,
-					R.layout.custom_row_textview, menuForMonth);
-
+					R.layout.custom_row_textview, menuForMonth, viewType);
 			ListView listView = (ListView) findViewById(R.id.listView1);
 
 			listView.setAdapter(adapter);
 			break;
 		case 1:
-			
-			//GridView g = (GridView) findViewById(R.id.grid);
-			//g.setAdapter(new ArrayAdapter<String>(this, R.layout.grid_cell, items));
-			//g.setOnItemClickListener(this);
+			CustomAdapter adapter2 = new CustomAdapter(this,
+					R.layout.custom_grid_textview, menuForMonth, viewType);
+			GridView g = (GridView) findViewById(R.id.grid);
+			g.setAdapter(adapter2);
+			// g.setAdapter(new ArrayAdapter<DailyMenu>(this,
+			// R.layout.grid_cell, menuForMonth));
+			// g.setOnItemClickListener(this);
 			break;
 		}
-		
 
 		/*
 		 * ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
@@ -162,28 +168,30 @@ public class MainActivity extends Activity {
 		// menuList.setOnItemClickListener(this);
 
 	}
-	public void startParsing(){
+
+	public void startParsing() {
 		obj = new JSONCalendarParser(url1, getApplicationContext());
 		obj.fetchJSON();
-		while (obj.parsingComplete);
+		while (obj.parsingComplete)
+			;
 
 		result = obj.getContentFromJson();
 
 		menuForMonth = obj.getMenuFromJson();
-		switch (viewType){
+		switch (viewType) {
 		case 0:
 			CustomAdapter adapter = new CustomAdapter(this,
-					R.layout.custom_row_textview, menuForMonth);
+					R.layout.custom_row_textview, menuForMonth, viewType);
 
 			ListView listView = (ListView) findViewById(R.id.listView1);
 
 			listView.setAdapter(adapter);
 			break;
 		case 1:
-			
-			//GridView g = (GridView) findViewById(R.id.grid);
-			//g.setAdapter(new ArrayAdapter<String>(this, R.layout.grid_cell, items));
-			//g.setOnItemClickListener(this);
+			GridView g = (GridView) findViewById(R.id.grid);
+			g.setAdapter(new ArrayAdapter<DailyMenu>(this, R.layout.grid_cell,
+					menuForMonth));
+			// g.setOnItemClickListener(this);
 			break;
 		}
 	}
