018346c948419501f3934165c6357d989ead5a54
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-07-16 14:38:24 -0400

    Shared Prefs

--- src/com/zos/lunchcalendar/SettingsActivity.java
+++ src/com/zos/lunchcalendar/SettingsActivity.java
@@ -3,10 +3,16 @@ package com.zos.lunchcalendar;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.SparseBooleanArray;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,12 +29,22 @@ import android.widget.Toast;
 
 public class SettingsActivity extends ActionBarActivity {
 
-	TextView mealsList;
+	
 	private JSONCalendarParser obj;
 	private ArrayList<String> mealsListText = new ArrayList<String>();
 	private ArrayList<DailyMenu> menuForMonth = new ArrayList<DailyMenu>();
-
+	
+	TextView mealsList;
+	MultiSelectionSpinner daySpinner;
 	ListView mealsListView;
+	Spinner timeSpinner;
+
+	ArrayList<String> preferredMeals = new ArrayList<String>();
+	String preferredTime;
+	ArrayList<String> preferredDays = new ArrayList<String>();
+	
+	Set preferredMealsFromArray = new HashSet();
+	Set preferredDaysFromArray = new HashSet();
 
 	Button save;
 
@@ -39,6 +55,11 @@ public class SettingsActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_settings);
 		// mealsList = (TextView)findViewById(R.id.mealsList);
+
+		String[] daysArray = { "Sunday", "Monday", "Tuesday", "Wednesday",
+				"Thursday", "Friday" };
+		daySpinner = (MultiSelectionSpinner) findViewById(R.id.daySpinner);
+		daySpinner.setItems(daysArray);
 
 		AssetManager am = getApplicationContext().getAssets();
 		/*
@@ -67,7 +88,6 @@ public class SettingsActivity extends ActionBarActivity {
 		mealsListText = obj.getContentFromJson();
 		ArrayList<String> mealz = new ArrayList<String>();
 
-		// add elements to al, including duplicates
 		HashSet<String> hs = new HashSet<String>();
 
 		for (int i = 0; i < mealsListText.size(); i++) {
@@ -83,8 +103,6 @@ public class SettingsActivity extends ActionBarActivity {
 			mealz.addAll(hs);
 		}
 
-		// mealsList.setText(mealz.get(1));
-
 		mealsListView = (ListView) findViewById(R.id.favoriteMeals);
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_multiple_choice, mealz);
@@ -93,35 +111,122 @@ public class SettingsActivity extends ActionBarActivity {
 		mealsListView.setItemsCanFocus(false);
 		mealsListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
+		timeSpinner = (Spinner) findViewById(R.id.timeSpinner);
+
+
 		save = (Button) findViewById(R.id.saveSettings);
 		save.setOnClickListener(new View.OnClickListener() {
-		String checked = "";
-		String unChecked = "";
+
 			@Override
 			public void onClick(View v) {
 
 				// TODO Auto-generated method stub
-				SparseBooleanArray sparseBooleanArray = mealsListView.getCheckedItemPositions();
+				SparseBooleanArray sparseBooleanArray = mealsListView
+						.getCheckedItemPositions();
+
 				
+
 				for (int i = 0; i < mealsListView.getCount(); i++) {
-					if(sparseBooleanArray.get(i) == true) 
-				     {
-				         checked += mealsListView.getItemAtPosition(i).toString() + "\n";
-				         Toast.makeText(getApplicationContext(), checked, Toast.LENGTH_SHORT).show();
-				     }
-					
-					//View vListSortOrder;
-					//vListSortOrder = mealsListView.getChildAt(i);
-					
-					
-					//EditText edit = (EditText) vListSortOrder
-						//	.findViewById(R.id.share_comment_edit);
-					//String temp = edit.getText().toString();
+					if (sparseBooleanArray.get(i) == true) {
+						preferredMeals.add(mealsListView.getItemAtPosition(i)
+								.toString());
+					}
 				}
+				
+				/** Save to shared preferences **/
+				preferredTime = timeSpinner.getSelectedItem().toString();
+			
+				preferredDays = daySpinner.getSelectedArrayStrings();
+				
+				savePreferences("PreferredMeals", preferredMeals);
+				savePreferences("PreferredTime", preferredTime);
+				savePreferences("PreferredDays", preferredDays);
+				
+				/*savePreferences("CheckBox_Value", checkBox.isChecked());
+				if (checkBox.isChecked())
+					savePreferences("storedName", editText.getText().toString());*/
+
+				finish();
+
 			}
 		});
+		loadSavedPreferences();
 
 	}
+
+	@SuppressLint("NewApi") 
+	private void loadSavedPreferences() {
+		SharedPreferences sharedPreferences = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		
+		preferredMealsFromArray = sharedPreferences.getStringSet("PreferredMeals", null);
+		Iterator<String> itr = preferredMealsFromArray.iterator();
+		
+        while(itr.hasNext()){	//from hashSet to ArrayString
+            preferredMeals.add(itr.next());
+        }
+        
+        preferredTime = sharedPreferences.getString("PreferredTime", "06:00");
+        
+        
+        preferredDaysFromArray = sharedPreferences.getStringSet("PreferredDays", null);
+		Iterator<String> itr2 = preferredDaysFromArray.iterator();
+		
+        while(itr.hasNext()){	//from hashSet to ArrayString
+            preferredDays.add(itr2.next());
+        }
+        
+		/*boolean checkBoxValue = sharedPreferences.getBoolean("CheckBox_Value", false);
+		String name = sharedPreferences.getString("storedName", "YourName");
+		if (checkBoxValue) {
+			checkBox.setChecked(true);
+		} else {
+			checkBox.setChecked(false);
+		}
+
+		editText.setText(name);*/
+	}
+
+	private void savePreferences(String key, boolean value) {
+		SharedPreferences sharedPreferences = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		Editor editor = sharedPreferences.edit();
+		editor.putBoolean(key, value);
+		editor.commit();
+	}
+
+	private void savePreferences(String key, String value) {
+		SharedPreferences sharedPreferences = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		
+		Editor editor = sharedPreferences.edit();
+		editor.putString(key, value);
+		editor.commit();
+	}
+	
+	@SuppressLint("NewApi") 
+	private void savePreferences(String key, ArrayList<String> value) {
+		SharedPreferences sharedPreferences = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		Set preferredMealsSet = new HashSet();
+		
+		for (int i = 0; i < value.size(); i++)
+			preferredMealsSet.add(value.get(i));
+		
+		Editor editor = sharedPreferences.edit();
+		editor.putStringSet("PreferredMeals", preferredMealsSet);
+		editor.commit();
+	}
+
+
+	/*
+	 * public void onClick(View v) { // TODO Auto-generated method stub
+	 * savePreferences("CheckBox_Value", checkBox.isChecked()); if
+	 * (checkBox.isChecked()) savePreferences("storedName",
+	 * editText.getText().toString());
+	 * 
+	 * finish(); }
+	 */
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -140,5 +245,10 @@ public class SettingsActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	public void onClickDD(View v) {
+		String s = daySpinner.getSelectedItemsAsString();
+		Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
 	}
 }
