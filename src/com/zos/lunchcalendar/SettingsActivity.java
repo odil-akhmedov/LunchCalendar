9cece7821a64a2cec358485a7a4f3fbfeeccf27d
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-07-19 14:25:27 -0400

    spinner_spinner

--- src/com/zos/lunchcalendar/SettingsActivity.java
+++ src/com/zos/lunchcalendar/SettingsActivity.java
@@ -36,6 +36,7 @@ public class SettingsActivity extends ActionBarActivity {
 
 	TextView mealsList;
 	//MultiSelectionSpinner daySpinner;
+	MultiSelectionSpinner mealSpinner;
 	Spinner daySpinner;
 	ListView mealsListView;
 	Spinner timeSpinner;
@@ -67,6 +68,8 @@ public class SettingsActivity extends ActionBarActivity {
 		String[] daysArray = { "Two day Before", "A day before", "Same day"};
 		//daySpinner = (MultiSelectionSpinner) findViewById(R.id.daySpinner);
 		//daySpinner.setItems(daysArray);
+		mealSpinner = (MultiSelectionSpinner) findViewById(R.id.mealSpinner);
+		mealSpinner.setItems(daysArray);
 		timeSpinner = (Spinner) findViewById(R.id.timeSpinner);
 		daySpinner = (Spinner) findViewById(R.id.daySpinner);
 
